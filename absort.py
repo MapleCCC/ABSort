43839c25f494fc8ca52c4880a73c69a6a9dcacde
MapleCCC <littlelittlemaple@gmail.com>
2020-08-08 02:19:17 +0800

    Performance improvement: only initialize temporary directory for once.

--- absort.py
+++ absort.py
@@ -19,24 +19,28 @@ DECL_STMT_CLASSES = (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)
 TYPE_DECL_STMT = Union[ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef]
 
 
+tempdir = TemporaryDirectory()
+# FIXME No need to explicitly cleanup.
+atexit.register(tempdir.cleanup)
+tempfile = Path(tempdir.name) / "file.py"
+
+
 def get_dependency_of_decl(decl: TYPE_DECL_STMT) -> Set[str]:
     src = astor.to_source(decl)
     deps = set()
-    with TemporaryDirectory() as tempdir:
-        tempfile = Path(tempdir) / "file.py"
-        tempfile.write_text(src, encoding="utf-8")
-        # cli = f"{tempfile} --errors-only --output-format=parseable --from-stdin \n{src}"
-        cli = f"{tempfile} --errors-only --output-format=parseable"
-        lint_stdout, lint_stderr = epylint.py_run(cli, return_std=True)  # type: ignore
-        out = lint_stdout.read()
-        err = lint_stderr.read()
-        assert not err
-        for line in out.splitlines():
-            pattern = r".*: error \(E0602, undefined-variable, (?P<declname>\w+)\) Undefined variable '(?P<dependency>\w+)'"
-            matchobj = re.fullmatch(pattern, line)
-            if matchobj:
-                deps.add(matchobj.group("dependency"))
-        return deps
+    tempfile.write_text(src, encoding="utf-8")
+    # cli = f"{tempfile} --errors-only --output-format=parseable --from-stdin \n{src}"
+    cli = f"{tempfile} --errors-only --output-format=parseable"
+    lint_stdout, lint_stderr = epylint.py_run(cli, return_std=True)  # type: ignore
+    out = lint_stdout.read()
+    err = lint_stderr.read()
+    assert not err
+    for line in out.splitlines():
+        pattern = r".*: error \(E0602, undefined-variable, (?P<declname>\w+)\) Undefined variable '(?P<dependency>\w+)'"
+        matchobj = re.fullmatch(pattern, line)
+        if matchobj:
+            deps.add(matchobj.group("dependency"))
+    return deps
 
 
 def absort_decls(decls: List[TYPE_DECL_STMT]) -> List[TYPE_DECL_STMT]:
