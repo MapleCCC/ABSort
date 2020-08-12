7c22d8bde729f02ca7c8ff8ec0eff45da8328603
MapleCCC <littlelittlemaple@gmail.com>
2020-08-12 16:57:44 +0800

    Proper renaming

--- absort.py
+++ absort.py
@@ -33,45 +33,43 @@ def get_descendant_names(node: ast.AST) -> Set[str]:
     return identifiers
 
 
-# TODO rename dependency to undefined-variable/name/symbol
 # TODO order by their appearance in https://docs.python.org/3/library/ast.html#abstract-grammar
 # TODO add runtime type checking
 # TODO fill in docstring to elaborate on details.
-class GetDependencyVisitor(ast.NodeVisitor):
+class GetUndefinedVariableVisitor(ast.NodeVisitor):
     """
 
     """
 
     def __init__(self) -> None:
         super().__init__()
-        self._deps: Set[str] = set()
-        self._env_list: List[Set[str]] = []
+        self._undefined_vars: Set[str] = set()
+        self._symbol_table_stack: List[Set[str]] = []
 
-    __slots__ = ("_deps", "_env_list")
+    __slots__ = ("_undefined_vars", "_symbol_table_stack")
 
-    # FIXME Should not use verb as name for a @property function
     @property
-    def get_dependency(self) -> Set[str]:
-        return self._deps
+    def undefined_variables(self) -> Set[str]:
+        return self._undefined_vars
 
     # FIXME no need to traverse in reverse order
-    def _env_lookup(self, name: str) -> bool:
-        for env in self._env_list[::-1]:
-            if name in env:
+    def _symbol_table_lookup(self, name: str) -> bool:
+        for symbol_table in self._symbol_table_stack[::-1]:
+            if name in symbol_table:
                 return True
         return False
 
     def visit_Name(self, node: ast.Name) -> None:
         if isinstance(node.ctx, ast.Store):
-            if self._env_lookup(node.id):
+            if self._symbol_table_lookup(node.id):
                 pass
             else:
-                self._env_list[-1].add(node.id)
+                self._symbol_table_stack[-1].add(node.id)
         elif isinstance(node.ctx, ast.Load):
-            if self._env_lookup(node.id):
+            if self._symbol_table_lookup(node.id):
                 pass
             else:
-                self._deps.add(node.id)
+                self._undefined_vars.add(node.id)
         else:
             # TODO fill in cases for contexts Del, AugLoad, AugStore, Param
             raise NotImplementedError
@@ -81,7 +79,7 @@ class GetDependencyVisitor(ast.NodeVisitor):
     ########################################################################
 
     def visit_Module(self, node: ast.Module) -> None:
-        self._env_list = []
+        self._symbol_table_stack = []
         for stmt in node.body:
             self.visit(stmt)
 
@@ -90,34 +88,34 @@ class GetDependencyVisitor(ast.NodeVisitor):
         for decorator in node.decorator_list:
             self.visit(decorator)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         arg_names = get_funcdef_arg_names(node)
-        self._env_list[-1].update(arg_names)
+        self._symbol_table_stack[-1].update(arg_names)
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
-        self._env_list[-1].add(node.name)
+        self._symbol_table_stack[-1].add(node.name)
 
     # TODO what is the node.returns attribute?
     def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> None:
         for decorator in node.decorator_list:
             self.visit(decorator)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         arg_names = get_funcdef_arg_names(node)
-        self._env_list[-1].update(arg_names)
+        self._symbol_table_stack[-1].update(arg_names)
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
-        self._env_list[-1].add(node.name)
+        self._symbol_table_stack[-1].add(node.name)
 
     # TODO what is the node.keywords attribute?
     def visit_ClassDef(self, node: ast.ClassDef) -> None:
@@ -127,82 +125,82 @@ class GetDependencyVisitor(ast.NodeVisitor):
         for base in node.bases:
             self.visit(base)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
-        self._env_list[-1].add(node.name)
+        self._symbol_table_stack[-1].add(node.name)
 
     def visit_For(self, node: ast.For) -> None:
         self.visit(node.iter)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         # FIXME I am not sure this is correct
         target_names = get_descendant_names(node.target)
-        self._env_list[-1].update(target_names)
+        self._symbol_table_stack[-1].update(target_names)
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list[-1].clear()
+        self._symbol_table_stack[-1].clear()
 
         for stmt in node.orelse:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_AsyncFor(self, node: ast.AsyncFor) -> None:
         self.visit(node.iter)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         # FIXME I am not sure this is correct
         target_names = get_descendant_names(node.target)
-        self._env_list[-1].update(target_names)
+        self._symbol_table_stack[-1].update(target_names)
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list[-1].clear()
+        self._symbol_table_stack[-1].clear()
 
         for stmt in node.orelse:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_While(self, node: ast.While) -> None:
         self.visit(node.test)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list[-1].clear()
+        self._symbol_table_stack[-1].clear()
 
         for stmt in node.orelse:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_If(self, node: ast.If) -> None:
         self.visit(node.test)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list[-1].clear()
+        self._symbol_table_stack[-1].clear()
 
         for stmt in node.orelse:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_With(self, node: ast.With) -> None:
         introduced_names: Set[str] = set()
@@ -216,14 +214,14 @@ class GetDependencyVisitor(ast.NodeVisitor):
                 optional_var_names = get_descendant_names(optional_vars)
                 introduced_names.update(optional_var_names)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
-        self._env_list[-1].update(introduced_names)
+        self._symbol_table_stack[-1].update(introduced_names)
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_AsyncWith(self, node: ast.AsyncWith) -> None:
         introduced_names: Set[str] = set()
@@ -237,22 +235,22 @@ class GetDependencyVisitor(ast.NodeVisitor):
                 optional_var_names = get_descendant_names(optional_vars)
                 introduced_names.update(optional_var_names)
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
-        self._env_list[-1].update(introduced_names)
+        self._symbol_table_stack[-1].update(introduced_names)
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_Try(self, node: ast.Try) -> None:
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         for stmt in node.body:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
         handlers = node.handlers
 
@@ -260,54 +258,54 @@ class GetDependencyVisitor(ast.NodeVisitor):
             if handler.type:
                 self.visit(handler.type)
 
-            self._env_list.append(set())
+            self._symbol_table_stack.append(set())
 
             if handler.name:
-                self._env_list[-1].add(handler.name)
+                self._symbol_table_stack[-1].add(handler.name)
 
             for stmt in handler.body:
                 self.visit(stmt)
 
-            self._env_list.pop()
+            self._symbol_table_stack.pop()
 
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         for stmt in node.orelse:
             self.visit(stmt)
 
-        self._env_list[-1].clear()
+        self._symbol_table_stack[-1].clear()
 
         for stmt in node.finalbody:
             self.visit(stmt)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_Lambda(self, node: ast.Lambda) -> None:
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         arg_names = get_funcdef_arg_names(node)
-        self._env_list[-1].update(arg_names)
+        self._symbol_table_stack[-1].update(arg_names)
 
         self.visit(node.body)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_IfExp(self, node: ast.IfExp) -> None:
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
 
         self.visit(node.body)
 
-        self._env_list[-1].clear()
+        self._symbol_table_stack[-1].clear()
 
         self.visit(node.orelse)
 
-        self._env_list.pop()
+        self._symbol_table_stack.pop()
 
     def visit_ListComp(self, node: ast.ListComp) -> None:
-        self._env_list.append(set())
+        self._symbol_table_stack.append(set())
         for generator in node.generators:
             pass
-        self._env_list[-1]
+        self._symbol_table_stack[-1]
 
     ########################################################################
     # Handle stmts that introduce new symbols, or delete existing symbols
@@ -322,57 +320,57 @@ class GetDependencyVisitor(ast.NodeVisitor):
         for name in to_delete_names:
             # FIXME we need to change the lookup scope to the scopes that are
             # allowed by the `del` keyword, as per the Python language grammar.
-            if name in self._env_list[-1]:
-                self._env_list[-1].remove(name)
+            if name in self._symbol_table_stack[-1]:
+                self._symbol_table_stack[-1].remove(name)
             else:
                 # there is no corresponding name in local scope
-                self._deps.add(name)
+                self._undefined_vars.add(name)
 
     def visit_Import(self, node: ast.Import) -> None:
         aliases = node.names
         for alias in aliases:
             if alias.asname:
-                self._env_list[-1].add(alias.asname)
+                self._symbol_table_stack[-1].add(alias.asname)
             else:
-                self._env_list[-1].add(alias.name)
+                self._symbol_table_stack[-1].add(alias.name)
 
     def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
         aliases = node.names
         for alias in aliases:
             if alias.asname:
-                self._env_list[-1].add(alias.asname)
+                self._symbol_table_stack[-1].add(alias.asname)
             else:
-                self._env_list[-1].add(alias.name)
+                self._symbol_table_stack[-1].add(alias.name)
 
     def visit_Global(self, node: ast.Global) -> None:
         for name in node.names:
             # FIXME we need to narrow down the lookup scope to global scope, the scope
             # that are allowed by the `global` keyword, as per the Python language
             # grammar.
-            if self._env_lookup(name):
-                self._env_list[-1].add(name)
+            if self._symbol_table_lookup(name):
+                self._symbol_table_stack[-1].add(name)
             else:
                 # there is no corresponding name in outter scopes
-                self._deps.add(name)
+                self._undefined_vars.add(name)
 
     def visit_Nonlocal(self, node: ast.Nonlocal) -> None:
         for name in node.names:
             # FIXME we need to narrow down the lookup scope to the scopes that are
             # allowed by the `nonlocal` keyword, as per the Python language grammar.
-            if self._env_lookup(name):
-                self._env_list[-1].add(name)
+            if self._symbol_table_lookup(name):
+                self._symbol_table_stack[-1].add(name)
             else:
                 # there is no corresponding name in outter scopes
-                self._deps.add(name)
+                self._undefined_vars.add(name)
 
     # TODO fill and complete
 
 
 def get_dependency_of_decl(decl: TYPE_DECL_STMT) -> Set[str]:
-    visitor = GetDependencyVisitor()
+    visitor = GetUndefinedVariableVisitor()
     temp_module = ast.Module(body=[decl])
     visitor.visit(temp_module)
-    return visitor.get_dependency
+    return visitor.undefined_variables
 
 
 def absort_decls(decls: List[TYPE_DECL_STMT]) -> List[TYPE_DECL_STMT]:
