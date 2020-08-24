65cefb031c802b37354b1e8abc48356409b28669
MapleCCC <littlelittlemaple@gmail.com>
2020-08-25 03:19:39 +0800

    Extract LRU data structure to standalone module

---
+++ absort/lru.py
@@ -0,0 +1,49 @@
+import math
+from collections import deque
+from typing import Any, Deque, Dict, Optional
+
+from .utils import add_profile_decorator_to_class_methods
+
+
+__all__ = ["LRU"]
+
+
+@add_profile_decorator_to_class_methods
+class LRU:
+    def __init__(self, maxsize: Optional[int] = 128) -> None:
+        if maxsize is None:
+            maxsize = math.inf  # type: ignore
+        if maxsize <= 0:
+            raise ValueError("maxsize should be positive integer")
+        self._maxsize = maxsize
+        self._storage: Dict = dict()
+        self._recency: Deque = deque()
+
+    __slots__ = ("_maxsize", "_storage", "_recency")
+
+    @property
+    def size(self) -> int:
+        return len(self._recency)
+
+    def update(self, key: Any, value: Any) -> None:
+        if key in self._storage:
+            self._recency.remove(key)
+        self._recency.append(key)
+        self._storage[key] = value
+
+        if len(self._recency) > self._maxsize:
+            to_evict = self._recency.popleft()
+            del self._storage[to_evict]
+
+    def __contains__(self, key: Any) -> bool:
+        return key in self._storage
+
+    def __getitem__(self, key: Any) -> Any:
+        try:
+            return self._storage[key]
+        except KeyError:
+            raise KeyError(f"Key {key} is not in LRU")
+
+    def clear(self) -> None:
+        self._storage.clear()
+        self._recency.clear()
--- absort/utils.py
+++ absort/utils.py
@@ -2,18 +2,15 @@ import contextlib
 import difflib
 import functools
 import io
-import math
 import os
 import sys
 import tokenize
-from collections import deque, namedtuple
+from collections import namedtuple
 from pathlib import Path
 from typing import (
     IO,
     Any,
     Callable,
-    Deque,
-    Dict,
     Iterable,
     Iterator,
     List,
@@ -22,6 +19,8 @@ from typing import (
 )
 
 from colorama import Fore, Style
+
+from .lru import LRU
 
 __all__ = [
     "ireverse",
@@ -146,47 +145,6 @@ def silent_context() -> Iterator:
         yield
     finally:
         sys.stdout = original_stdout
-
-
-@add_profile_decorator_to_class_methods
-class LRU:
-    def __init__(self, maxsize: Optional[int] = 128) -> None:
-        if maxsize is None:
-            maxsize = math.inf  # type: ignore
-        if maxsize <= 0:
-            raise ValueError("maxsize should be positive integer")
-        self._maxsize = maxsize
-        self._storage: Dict = dict()
-        self._recency: Deque = deque()
-
-    __slots__ = ("_maxsize", "_storage", "_recency")
-
-    @property
-    def size(self) -> int:
-        return len(self._recency)
-
-    def update(self, key: Any, value: Any) -> None:
-        if key in self._storage:
-            self._recency.remove(key)
-        self._recency.append(key)
-        self._storage[key] = value
-
-        if len(self._recency) > self._maxsize:
-            to_evict = self._recency.popleft()
-            del self._storage[to_evict]
-
-    def __contains__(self, key: Any) -> bool:
-        return key in self._storage
-
-    def __getitem__(self, key: Any) -> Any:
-        try:
-            return self._storage[key]
-        except KeyError:
-            raise KeyError(f"Key {key} is not in LRU")
-
-    def clear(self) -> None:
-        self._storage.clear()
-        self._recency.clear()
 
 
 def lru_cache_with_key(
