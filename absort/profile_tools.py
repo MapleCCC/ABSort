52cc50bcafa6453d451b46e1115292b40568822a
MapleCCC <littlelittlemaple@gmail.com>
2020-10-11 16:33:57 +0800

    Fix circular imports

--- absort/profile_tools.py
+++ absort/profile_tools.py
@@ -1,9 +1,15 @@
-from typing import Callable, Type
-
-from .utils import identityfunc
+from typing import Callable, Type, TypeVar
 
 
 __all__ = ["add_profile_decorator_to_class_methods"]
+
+
+T = TypeVar("T")
+
+# Duplicate the definition of utils.identityfunc() here, to workaround circular imports
+def identityfunc(input: T) -> T:
+    """ An identity function """
+    return input
 
 
 # TODO how to use type annotation to specify that the input Type is same with the output Type?
