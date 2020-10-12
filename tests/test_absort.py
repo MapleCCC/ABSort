0da7575d0a1f8fade386dcb95889bc9addb3410f
MapleCCC <littlelittlemaple@gmail.com>
2020-10-13 01:17:24 +0800

    Instead of testing all option combination on every test file, we pick a random one from all option combinations every time.

--- tests/test_absort.py
+++ tests/test_absort.py
@@ -1,4 +1,5 @@
 import ast
+import random
 import sys
 from itertools import combinations_with_replacement, product
 from pathlib import Path
@@ -19,31 +20,31 @@ TEST_FILES = Path(sys.executable).with_name("Lib").glob("*.py")
 
 
 def test_absort_str() -> None:
+    all_comment_strategies = iter(CommentStrategy)
+    format_option_init_arg_options = combinations_with_replacement(
+        (True, False), len(attr.fields(FormatOption))
+    )
+    all_format_options = (
+        FormatOption(*c) for c in format_option_init_arg_options  # type: ignore
+    )
+    all_arg_options = list(product(all_comment_strategies, all_format_options))
+
     for test_sample in TEST_FILES:
         try:
-            all_comment_strategies = iter(CommentStrategy)
-            format_option_init_arg_options = combinations_with_replacement(
-                (True, False), len(attr.fields(FormatOption))
+            source = test_sample.read_text(encoding="utf-8")
+            arg_option = random.choice(all_arg_options)
+            comment_strategy, format_option = arg_option
+            new_source = absort_str(
+                source,
+                comment_strategy=comment_strategy,
+                format_option=format_option,
             )
-            all_format_options = (
-                FormatOption(*c) for c in format_option_init_arg_options  # type: ignore
-            )
-            all_arg_options = product(all_comment_strategies, all_format_options)
 
-            for arg_option in all_arg_options:
-                comment_strategy, format_option = arg_option
-                source = test_sample.read_text(encoding="utf-8")
-                new_source = absort_str(
-                    source,
-                    comment_strategy=comment_strategy,
-                    format_option=format_option,
-                )
+            old_ast = ast.parse(source)
+            new_ast = ast.parse(new_source)
 
-                old_ast = ast.parse(source)
-                new_ast = ast.parse(new_source)
-
-                assert len(old_ast.body) == len(new_ast.body)
-                for stmt in old_ast.body:
-                    assert contains(new_ast.body, stmt, equal=ast_deep_equal)
+            assert len(old_ast.body) == len(new_ast.body)
+            for stmt in old_ast.body:
+                assert contains(new_ast.body, stmt, equal=ast_deep_equal)
         except (SyntaxError, NameRedefinition):
             pass
