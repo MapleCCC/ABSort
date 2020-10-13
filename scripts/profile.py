2a56658a7d6ab58fc12470c5d82d4673f3c7b25d
MapleCCC <littlelittlemaple@gmail.com>
2020-10-13 17:47:40 +0800

    Python 3.9 add ast.unparse(), rendering astor library unnecessary

--- scripts/profile.py
+++ scripts/profile.py
@@ -7,8 +7,6 @@ from shutil import copy2
 from subprocess import CalledProcessError
 from tempfile import TemporaryDirectory
 
-# TODO remove the astor dependency after the ast.unparse() land on Python 3.9
-import astor
 
 ISORT_MAIN_FILEPATH = "D:/Program Files/Python39/Lib/site-packages/isort/main.py"
 ISORT_CODE_DIR = "D:/Program Files/Python39/Lib/site-packages/isort"
@@ -63,7 +61,7 @@ def preprocess(p: Path) -> None:
 
     new_tree = ast.fix_missing_locations(new_tree)
 
-    new_content = astor.to_source(new_tree)
+    new_content = ast.unparse(new_tree)
 
     p.write_text(new_content, encoding="utf-8")
 
