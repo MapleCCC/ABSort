93f947fb74cdf382dd9b9c774e37a344e0f6e706
MapleCCC <littlelittlemaple@gmail.com>
2020-10-17 18:56:46 +0800

    Add unit test for chenyu()

---
+++ tests/strategies.py
@@ -0,0 +1,49 @@
+from hypothesis.strategies import (
+    binary,
+    booleans,
+    characters,
+    complex_numbers,
+    dates,
+    datetimes,
+    decimals,
+    floats,
+    fractions,
+    from_type,
+    functions,
+    integers,
+    none,
+    one_of,
+    text,
+    timedeltas,
+    times,
+)
+
+from absort.utils import constantfunc
+
+__all__ = ["anys", "hashables"]
+
+
+anys = constantfunc(from_type(type))
+
+
+# TODO add more hashable types to draw from.
+# Remember that the less complex one should be located at the front. Quote "In order to get good shrinking behaviour, try to put simpler strategies first" from https://hypothesis.readthedocs.io/en/latest/data.html#hypothesis.strategies.one_of
+hashables = constantfunc(
+    one_of(
+        none(),
+        booleans(),
+        integers(),
+        characters(),
+        text(),
+        binary(),
+        fractions(),
+        decimals(),
+        floats(),
+        complex_numbers(),
+        timedeltas(),
+        times(),
+        dates(),
+        datetimes(),
+        functions(),
+    )
+)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,28 @@
+from collections.abc import Hashable
+
 from hypothesis import given
 from hypothesis.strategies import integers, lists, tuples
 
 from absort.utils import chenyu, iequal
 
+from .strategies import hashables
+
 
 @given(lists(tuples(integers(), integers())), integers(min_value=2))
-def test_chenyu_is_deterministic(points: list[tuple[int, int]], k: int)->None:
+def test_chenyu_is_deterministic_plane_points(
+    points: list[tuple[int, int]], k: int
+) -> None:
     distance = lambda x, y: (x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2
+
+    clusters1 = chenyu(points, distance, k)
+    clusters2 = chenyu(points, distance, k)
+
+    assert iequal(clusters1, clusters2, strict=True)
+
+
+@given(lists(hashables()), integers(min_value=2))
+def test_chenyu_is_deterministic_hashables(points: list[Hashable], k: int) -> None:
+    distance = lambda x, y: abs(hash(x) - hash(y))
 
     clusters1 = chenyu(points, distance, k)
     clusters2 = chenyu(points, distance, k)
