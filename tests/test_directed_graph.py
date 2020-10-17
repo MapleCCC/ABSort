16b19f6d9d61c0195e7e055f73b377ff7ebc1fa7
MapleCCC <littlelittlemaple@gmail.com>
2020-10-17 23:43:04 +0800

    Add unit test for topological_sort(reverse=True)

--- tests/test_directed_graph.py
+++ tests/test_directed_graph.py
@@ -2,7 +2,7 @@ from collections.abc import Hashable
 from itertools import combinations
 
 from hypothesis import given
-from hypothesis.strategies import floats, from_type, integers, lists, one_of
+from hypothesis.strategies import booleans, floats, from_type, integers, lists, one_of
 
 from absort.directed_graph import DirectedGraph
 from absort.utils import constantfunc, iequal
@@ -18,9 +18,13 @@ hashables = constantfunc(one_of(integers()))  # TODO
 # TODO test on relaxed_topological_sort()
 
 
-@given(lists(hashables(), unique=True), lists(integers(min_value=0), unique=True))
+@given(
+    lists(hashables(), unique=True),
+    lists(integers(min_value=0), unique=True),
+    booleans(),
+)
 def test_topological_sort_is_deterministic(
-    node_pool: list[Hashable], indices: list[int]
+    node_pool: list[Hashable], indices: list[int], reverse: bool
 ) -> None:
     possible_edges = list(combinations(node_pool, 2))
 
@@ -35,18 +39,24 @@ def test_topological_sort_is_deterministic(
     graph1 = DirectedGraph()
     for edge in edges:
         graph1.add_edge(*edge)
-    result1 = graph1.topological_sort()
+    result1 = graph1.topological_sort(reverse=reverse)
 
     graph2 = DirectedGraph()
     for edge in edges:
         graph2.add_edge(*edge)
-    result2 = graph2.topological_sort()
+    result2 = graph2.topological_sort(reverse=reverse)
 
     assert iequal(result1, result2, strict=True)
 
 
-@given(lists(hashables(), unique=True), lists(integers(min_value=0), unique=True))
-def test_topological_sort_order(node_pool: list[Hashable], indices: list[int]) -> None:
+@given(
+    lists(hashables(), unique=True),
+    lists(integers(min_value=0), unique=True),
+    booleans(),
+)
+def test_topological_sort_order(
+    node_pool: list[Hashable], indices: list[int], reverse: bool
+) -> None:
     # No need to construct a connected graph, we only need to guarantee that it's acyclic.
 
     possible_edges = list(combinations(node_pool, 2))
@@ -64,10 +74,13 @@ def test_topological_sort_order(node_pool: list[Hashable], indices: list[int]) -
         graph.add_node(node)
     for edge in edges:
         graph.add_edge(*edge)
-    nodes = list(graph.topological_sort())
+    nodes = list(graph.topological_sort(reverse=reverse))
 
     assert len(nodes) == len(node_pool)
     assert set(nodes) == set(node_pool)
 
     for u, v in edges:
-        assert nodes.index(u) < nodes.index(v)
+        if not reverse:
+            assert nodes.index(u) < nodes.index(v)
+        else:
+            assert nodes.index(u) > nodes.index(v)
